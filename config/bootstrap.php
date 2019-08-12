a2318a36267629fd1d090c1888624dfcfac2fced
Andy Hoffner <ahoffner@gmail.com>
2019-08-12 15:24:00 -0600

    Removing alias, causes class name conflict.

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -6,6 +6,11 @@ use Cake\Core\Configure;
  * OAuthServer plugin creates controller that extends App\Controller\AppController class.
  * Config OAuthServer.appController allows to override the base controller class.
  */
+/**
+ * No. This is a terrible idea it redecalres AppController. Better to just .. call OAuthController wtf is this. Commenting out
+ 
 $appControllerReal = Configure::read('OAuthServer.appController') ?: 'App\Controller\AppController';
 $appControllerAlias = 'OAuthServer\Controller\AppController';
 class_alias($appControllerReal, $appControllerAlias);
+ *
+**/
